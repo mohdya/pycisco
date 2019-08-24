32e79c0c3a9574dde9ecccb8cdf3346e5d7d68b2
Yaseen M <yaseen_gem@yahoo.com>
2019-08-24 20:33:21 +1000

    2nd

---
+++ cisco.py
@@ -0,0 +1,25 @@
+from netmiko import ConnectHandler
+import time, sys, getpass, paramiko 
+
+print ("Before connecting")
+
+
+#device2 = ConnectHandler(device_type='cisco_ios', host='sbx-iosxr-mgmt.cisco.com', username='admin', password='C1sco12345', port=8181)
+#output = device2.send_command("show running-config | be line vty")
+#print (output)
+#device2.disconnect()
+
+
+
+device1 = ConnectHandler(device_type='cisco_ios', host='ios-xe-mgmt-latest.cisco.com', username='developer', password='C1sco12345', port=8181)
+output = device1.send_command("show running-config | be line vty")
+print (output)
+device1.disconnect()
+
+
+#configcmds=["line vty 0 4", "exec-timeout 0 0"] 
+#print (configcmds)
+#device.send_config_set(configcmds)
+
+print ("Clsoing")
+

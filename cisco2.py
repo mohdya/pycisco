9464827b38ca75b340a925ad193aa7251aef6cde
Yaseen M <yaseen_gem@yahoo.com>
2019-08-24 22:22:36 +1000

    Added file for inventory and for loop to read it.

--- cisco2.py
+++ cisco2.py
@@ -1,24 +1,29 @@
 from netmiko import ConnectHandler
 
-print ("Before connecting")
-device1 = ConnectHandler(device_type='cisco_ios', host='ios-xe-mgmt-latest.cisco.com', username='developer', password='C1sco12345', port=8181)
-output = device1.send_command("show running-config | be line vty")
-print (output)
+file = open('inv.txt', 'r') 
+for line in file: 
+    print ("Processing for the device: ", line)
+    print ("Before connecting")
+    device1 = ConnectHandler(device_type='cisco_ios', host=line.rstrip('\r\n'), username='developer', password='C1sco12345', port=8181, timeout=30 )
+    output = device1.send_command("show running-config | include exec-timeout")
+    print (output)
 
 
-print ("Configruing now")
-configcmds=["line vty 0 4", "exec-timeout 50 0"] 
-print (configcmds)
-device1.send_config_set(configcmds)
+    print ("Configruing now")
+    configcmds=["line vty 0 4", "exec-timeout 30 0"] 
+    print (configcmds)
+    device1.send_config_set(configcmds)
 
 
-print ("After configuring")
-output = device1.send_command("show running-config | be line vty")
-print (output)
+    print ("After configuring")
+    output = device1.send_command("show running-config | include exec-timeout")
+    print (output)
 
-print ("Closing")
+    print ("Closing Connection")
 
-device1.disconnect()
+    device1.disconnect()
 
 
 
+
+print ("End of Inventory")
